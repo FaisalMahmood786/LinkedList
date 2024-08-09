8870d550b0c95cffd1d7bdefdfc051e597de6dd3
Faisal Mahmood <Faisal.Mahmood@evolutionmoney.co.uk>
2024-08-09 12:29:21 +0100

    Implemented Insert: Insert a node at any position & PrintList: Print the string representation of each node in the list.

---
+++ LinkedList.Tests/LinkedListTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LinkedList.Tests
+{
+    [TestFixture]
+    public class LinkedListTests
+    {
+        private LinkedList<int> list;
+
+        [SetUp]
+        public void Setup()
+        {
+            list = new LinkedList<int>();
+        }
+
+        [Test]
+        public void Insert_AtHead_Position1()
+        {
+            list.Insert(1, 10);
+            Assert.AreEqual("10", list.PrintList());
+        }
+
+        [Test]
+        public void Insert_AtPosition2()
+        {
+            list.Insert(1, 10);
+            list.Insert(2, 20);
+            Assert.AreEqual("10 -> 20", list.PrintList());
+        }
+
+        [Test]
+        public void Insert_InMiddle()
+        {
+            list.Insert(1, 10);
+            list.Insert(2, 30);
+            list.Insert(2, 20); 
+            Assert.AreEqual("10 -> 20 -> 30", list.PrintList());
+        }
+    }
+}
---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+
+namespace LinkedList
+{
+    public class LinkedList<T>
+    {
+        private Node<T> head;
+
+        public LinkedList()
+        {
+            head = null;
+        }
+
+        // Insert a node at a specific position (1-based index)
+        public void Insert(int position, T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (position < 1)
+            {
+                throw new ArgumentOutOfRangeException("Position must be >= 1");
+            }
+
+            if (position == 1)
+            {
+                newNode.Next = head;
+                head = newNode;
+                return;
+            }
+
+            Node<T> current = head;
+            Node<T> previous = null;
+            int index = 1;
+
+            while (current != null && index < position)
+            {
+                previous = current;
+                current = current.Next;
+                index++;
+            }
+
+            if (index != position)
+            {
+                throw new ArgumentOutOfRangeException("Position is out of bounds");
+            }
+
+            newNode.Next = current;
+            previous.Next = newNode;
+        }
+        // Print the string representation of the list
+        public string PrintList()
+        {
+            if (head == null)
+            {
+                return string.Empty;
+            }
+
+            Node<T> current = head;
+            StringBuilder result = new StringBuilder();
+            while (current != null)
+            {
+                result.Append((current.Data));
+                if (current.Next != null)
+                {
+                    result.Append(" -> ");
+                }
+                current = current.Next;
+            }
+            return result.ToString();
+        }
+
+    }
+}
